88e7de5b4849f1d7e7dfc5755674f4e6c32b6ae9
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-04-29 01:17:07 +0530

    Time: 42 ms (99.13%), Space: 13.9 MB (97.57%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,28 +1,31 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         
-        i = 0
-        j = 0
-        star_j=-1
-        while i <len(s):
+        cur_string_index = 0
+        cur_pattern_index = 0
+        last_star = None
+        n = len(s)
+        while cur_string_index<n:
             
-            if j==len(p) or ((p[j] not in {"?","*"})  and p[j]!=s[i]):
-                if star_j==-1:
+            if cur_pattern_index>=len(p) or (s[cur_string_index]!=p[cur_pattern_index] and p[cur_pattern_index] not in {"*","?"}):
+                if last_star is None:
                     return False
-                j = star_j+1
-                star_i+=1
-                i = star_i
-            elif p[j]=="*":
-                star_j = j
-                star_i=i
-                j+=1
+                cur_pattern_index = last_star+1
+                cur_string_index = last_star_compared+1
+                last_star_compared = cur_string_index
+            elif p[cur_pattern_index] == "*":
+                last_star = cur_pattern_index
+                cur_pattern_index+=1
+                last_star_compared = cur_string_index
             else:
-                i+=1
-                j+=1
-            
-        while j < len(p):
-            if p[j]!="*":
+                cur_string_index+=1
+                cur_pattern_index+=1
+                
+        while cur_pattern_index<len(p):
+            if p[cur_pattern_index] != "*":
                 return False
-            j+=1
+            cur_pattern_index+=1
+        
         return True
+                
             
