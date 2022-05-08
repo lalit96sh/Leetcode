5bc66ba2a73801871fb4d79abd1758d62a174f3d
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-05-08 23:10:03 +0530

    Time: 184 ms, Memory: 14.7 MB - LeetHub

---
+++ minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,53 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        
+        tmap = collections.defaultdict(int)
+        
+        for tt in t:
+            tmap[tt]+=1
+        
+        keycnt = len(tmap)
+        
+        
+        mp = collections.defaultdict(int)
+        cnt=0
+        start = 0
+        anslen = float("inf")
+        ans = ""
+        for i,ss in enumerate(s):
+            
+            if tmap[ss]:
+                mp[ss]+=1
+                if tmap[ss]==mp[ss]:
+                    cnt+=1
+            # print(start,i,cnt,mp)
+            if cnt==keycnt:
+                
+                while start<=i and cnt==keycnt:
+                    if tmap[s[start]]:
+                        if tmap[s[start]]==mp[s[start]]:
+                            cnt-=1
+                        mp[s[start]]-=1
+                    
+                    if anslen>i-start+1:
+                        anslen = i-start+1
+                        ans = s[start:i+1]
+                    start+=1
+                while start<len(s) and not tmap[s[start]]:
+                    start+=1
+        return ans
+            
+            
+        
+        
+        
+        
+        
+        
+                
+        
+                
+                
+            
+                
+        
