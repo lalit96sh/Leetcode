16a749d151462f56f09bbe7ddd3f2da607f9c627
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-05-09 23:40:12 +0530

    Time: 41 ms, Memory: 13.9 MB - LeetHub

--- read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.py
+++ read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.py
@@ -4,20 +4,21 @@
 class Solution:
     def __init__(self):
         self.buf4 = [''] * 4
-        self.i4 = 0
-        self.n4 = 0
+        self.cur_pointer = 0
+        self.read4_pointer = 0
         
     def read(self, buf: List[str], n: int) -> int:
         i = 0
-        while i < n:
-            if self.i4 == self.n4:
-                self.i4 = 0
-                self.n4 = read4(self.buf4)
-                if self.n4 == 0:
+        while i<n:
+            if self.read4_pointer==self.cur_pointer:
+                self.cur_pointer = 0
+                self.read4_pointer = read4(self.buf4)
+                if self.read4_pointer==0:
                     break
-            buf[i] = self.buf4[self.i4]
-            self.i4 += 1
-            i += 1
+            buf[i] = self.buf4[self.cur_pointer]
+            self.cur_pointer+=1
+            i+=1
+        
         return i
         
         
