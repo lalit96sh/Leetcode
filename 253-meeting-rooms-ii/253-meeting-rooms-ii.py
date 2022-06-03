9fbc414e7a541924104cfdc0236b1a6843d7995a
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-06-04 00:38:50 +0530

    Time: 177 ms (5.42%), Space: 17.6 MB (49.01%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -3,18 +3,19 @@ class Solution:
         start = sorted([x[0] for x in intervals])
         end = sorted([x[1] for x in intervals])
         
-        n = len(start)
-        ans = [start[0]]
-        i = 1
+        r = 1
+        ans = 1
+        i=1
         j = 0
-        while i<n and j<n:
+        n = len(start)
+        while i < n and j<n:
             
-            if end[j]>start[i]:
-                ans.append(start[i])
-                i+=1
+            if end[j]<=start[i]:
+                r-=1
+                j+=1
             else:
                 i+=1
-                j+=1
-        return len(ans)
+                r+=1
+            ans=max(ans,r)
+        return ans
                 
-        
