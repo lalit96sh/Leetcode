a37bb1aae0fbabef799512d984e5387eeb7670df
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-06-04 00:18:20 +0530

    Time: 2932 ms, Memory: 19.9 MB - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,28 +1,30 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        
-        def f(start,end,k):
+        n = len(nums)
+        k = n-k
+        def helper(start,end):
             if start==end:
                 return nums[start]
+            
             pivot = end
             
-            i = start
             j = start
             
-            for j in range(start,end):
-                if nums[j]<nums[pivot]:
-                    nums[i],nums[j]=nums[j],nums[i]
-                    i+=1
+            for i in range(start,end):
+                if nums[i]<nums[pivot]:
+                    nums[i],nums[j] = nums[j],nums[i]
+                    j+=1
             
-            nums[i],nums[pivot] = nums[pivot],nums[i]
+            nums[j],nums[pivot] = nums[pivot],nums[j]
             
-            if i==k:
-                return nums[i]
-            if i>k:
-                return f(start,i-1,k)
-            else:
-                return f(i+1,end,k)
-        n = len(nums)   
-        return f(0,n-1,n-k)
+            if j==k:
+                return nums[j]
+            
+            if j<k:
+                return helper(j+1,end)
+            return helper(start,j-1)
+        
+        return helper(0,n-1)
+            
             
         
