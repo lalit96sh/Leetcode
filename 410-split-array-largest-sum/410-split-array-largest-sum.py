1e8bbc859508fd3ce53bbd8d6d2305d75847df80
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-06-16 21:27:26 +0530

    Time: 7513 ms (5.04%), Space: 20 MB (8.69%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,46 +1,37 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
         
-#         n = len(nums)
-#         memo = {}
-#         @lru_cache
-#         def dfs(parts,index):
-#             # if (parts,index) in memo:
-#             #     return memo[(parts,index)]
-#             if parts==1:
-#                 return sum(nums[index:])
-#             if parts==n-index:
-#                 return max(nums[index:])
-            
-#             mn = float("inf")
-#             cursum = 0
-            
-#             for i in range(index,n-parts+1):
-#                 cursum += nums[i]
-#                 mn = min(mn,max(cursum,dfs(parts-1,i+1)))
-#             # memo[(parts,index)] = mn
-#             return mn
         
-#         return dfs(m,0)
-    # class Solution:
-    # def splitArray(self, nums: List[int], m: int) -> int:
-        left = max(nums)
-        right = sum(nums)
         n = len(nums)
-        while left < right:
-            mid = left + (right - left) // 2
+        memo = {}
+        prefix = [0]*n
+        prefix[0] = nums[0]
+        for i in range(1,n):
+            prefix[i] = prefix[i-1]+nums[i]
             
-            m_now = 1
-            now_sum = 0
-            for num in nums:
-                now_sum+=num
-                if now_sum>mid:
-                    m_now+=1
-                    now_sum = num
-                    
-            if m_now <= m:
-                right = mid
-            else:
-                left = mid + 1
+        mx = [0]*n
+        mx[n-1] = nums[n-1]
+        for j in range(n-2,-1,-1):
+            mx[j] = max(nums[j],mx[j+1])
         
-        return left
+        # @lru_cache
+        def dfs(parts,index):
+            if (parts,index) in memo:
+                return memo[(parts,index)]
+            if parts==1:
+                return prefix[n-1] - prefix[index] + nums[index]
+            if parts==n-index:
+                return mx[index]
+            
+            mn = float("inf")
+            cursum = 0
+            
+            for i in range(index,n-parts+1):
+                cursum += nums[i]
+                mn = min(mn,max(cursum,dfs(parts-1,i+1)))
+                if mn<=cursum:
+                    break
+            memo[(parts,index)] = mn
+            return mn
+        
+        return dfs(m,0)
