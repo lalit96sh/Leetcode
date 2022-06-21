1cfc2fb459cbd14b82d3869302f6fe5cbbafe5bb
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-06-21 17:07:37 +0530

    Time: 352 ms (5.12%), Space: 14 MB (5.19%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.py
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -1,25 +1,19 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
         
-        h = Counter(secret)
-            
+        h = defaultdict(int)
         bulls = cows = 0
-        for idx, ch in enumerate(guess):
-            if ch in h:
-                # corresponding characters match
-                if ch == secret[idx]:
-                    # update the bulls
-                    bulls += 1
-                    # update the cows 
-                    # if all ch characters from secret 
-                    # were used up
-                    cows -= int(h[ch] <= 0)
-                # corresponding characters don't match
-                else:
-                    # update the cows
-                    cows += int(h[ch] > 0)
-                # ch character was used
-                h[ch] -= 1
-                
+
+        for idx, s in enumerate(secret):
+            g = guess[idx]
+            print("----------")
+            print(h)
+            if s == g: 
+                bulls += 1
+            else:
+                cows += int(h[s] < 0) + int(h[g] > 0)
+                h[s] += 1
+                h[g] -= 1
+            print(h)
         return "{}A{}B".format(bulls, cows)
                 
