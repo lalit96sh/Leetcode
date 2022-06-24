3946729b27adef90ef0c9760d671f36be9e65d96
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-06-24 23:52:33 +0530

    Time: 1200 ms (9.65%), Space: 20.2 MB (22.68%) - LeetHub

--- 305-number-of-islands-ii/305-number-of-islands-ii.py
+++ 305-number-of-islands-ii/305-number-of-islands-ii.py
@@ -1,43 +1,59 @@
-class Solution:
-    def numIslands2(self, m, n, positions):
-        ans = []
-        islands = Union()
-        for p in map(tuple, positions):
-            if islands.sz.get(p,0)==0:
-                islands.add(p)
-                for dp in (0, 1), (0, -1), (1, 0), (-1, 0):
-                    q = (p[0] + dp[0], p[1] + dp[1])
-                    if q in islands.id:
-                        islands.unite(p, q)
-            ans += [islands.count]
-        return ans
-
-class Union(object):
-    def __init__(self):
-        self.id = {}
-        self.sz = {}
-        self.count = 0
-
-    def add(self, p):
-        self.id[p] = p
-        self.sz[p] = 1
-        self.count += 1
-
-    def root(self, i):
-        while i != self.id[i]:
-            # self.id[i] = self.id[self.id[i]]
-            i = self.id[i]
-        return i
-
-    def unite(self, p, q):
-        i, j = self.root(p), self.root(q)
-        if i == j:
+class unionfind:
+    def __init__(self,m,n):
+        self.rows = m
+        self.cols = n
+        self.pos = collections.defaultdict(int)
+        self.parent = {(i,j):(i,j) for j in range(n) for i in range(m)}
+        self.rank = collections.defaultdict(int)
+        self.islands = 0
+    
+    def find(self,node):
+        
+        while self.parent[node]!=node:
+            node = self.parent[node]
+        
+        return node
+    
+    def add_island(self,node):
+        if self.pos[node]==1:
             return
-        if self.sz[i] > self.sz[j]:
-            i, j = j, i
-        self.id[i] = j
-        self.sz[j] += 1
-        self.count -= 1
+        x,y = node
+        self.islands+=1
+        self.pos[node] = 1
+        for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+            xx,yy = x+dx,y+dy
+            if not(0<=xx<self.rows and 0<=yy<self.cols) or self.pos[(xx,yy)]==0:
+                continue
+            find = self.find((xx,yy))
+            self.union(node,find)
+    
+    def union(self,rx,ry):
+        x = self.find(rx)
+        y = ry #self.find(y)
+        
+        if x!=y:
+            if self.rank[x]>self.rank[y]:
+                self.parent[y] = x
+            elif self.rank[x]<self.rank[y]:
+                self.parent[x] = y
+            else:
+                self.parent[y] = x
+                self.rank[x]+=1
+            self.islands-=1
+                
+
+    def get_islands(self):
+        return self.islands
+
+class Solution:
+    def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
+        
+        uf = unionfind(m,n)
+        ans = []
+        for node in positions:
+            uf.add_island(tuple(node))
+            ans.append(uf.get_islands())
+        return ans
         
         
         
