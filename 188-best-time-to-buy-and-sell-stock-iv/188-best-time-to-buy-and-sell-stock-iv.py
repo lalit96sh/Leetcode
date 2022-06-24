46f0386e1f9424eebb848a7af5427ea5989c644b
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-06-24 19:31:32 +0530

    Time: 115 ms (84.14%), Space: 13.8 MB (93.84%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if 2*k >= len(prices): 
+            return sum(max(0, prices[i]-prices[i-1]) for i in range(1, len(prices)))
+        dp = [0]*(k+1)
+        mn = [prices[0]]*(k+1)
+        
+        n = len(prices)
+        # print(mn)
+        # print(dp)
+        # print("--------------------")
+        for i in range(1,n):
+            for kk in range(1,k+1):
+                # print("transaction {} price {}".format(kk,i))
+                mn[kk] = min(mn[kk],prices[i]-dp[kk-1])
+                dp[kk] = max(dp[kk],prices[i]-mn[kk])
+                # print(mn)
+                # print(dp)
+            # print("--------------------")
+                
+        return dp[k]
+            
+        
+        
+        
