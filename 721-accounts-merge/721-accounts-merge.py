150d84b9ae24f4a9ee5d98de00d9ae1cacf69303
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-07-14 12:07:40 +0530

    Time: 322 ms (54.00%), Space: 17.7 MB (97.18%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -1,37 +1,29 @@
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+    def union(self, child, parent):
+        self.parents[self.find(child)] = self.find(parent)
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
 class Solution:
+    # 196 ms, 82.09%. 
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UF(len(accounts))
         
-        email_to_account_index_map = collections.defaultdict(list)
-        
-        for idx,ac in enumerate(accounts):
-            emails = ac[1:]
-            
+        # Creat unions between indexes
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
             for email in emails:
-                email_to_account_index_map[email].append(idx)
-                
-        vis = set()
-        def dfs(idx,emails):
-            
-            if idx in vis:
-                return
-            vis.add(idx)
-            
-            for email in accounts[idx][1:]:
-                emails.add(email)
-                
-                for account_idx in email_to_account_index_map[email]:
-                    dfs(account_idx,emails)
-                    
-        ans = []
-        for idx,account in enumerate(accounts):
-            if idx in vis:
-                continue
-                
-            name,emails = account[0],set()
-            
-            dfs(idx,emails)
-            
-            ans.append([name]+sorted(emails))
-            
-        return ans
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
         
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
