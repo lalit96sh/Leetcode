4d0ed4d3d34f39d7e26bc3f12d21e636485a1a63
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-07-15 22:59:14 +0530

    Time: 1189 ms (98.09%), Space: 26 MB (6.16%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -5,12 +5,28 @@ class Solution:
         st = []
         ans = [0]*n
         
-        for i in range(n):
+#         for i in range(n):
             
-            while st and temperatures[st[-1]]<temperatures[i]:
-                j = st.pop()
-                ans[j] = i-j
-            st.append(i)
+#             while st and temperatures[st[-1]]<temperatures[i]:
+#                 j = st.pop()
+#                 ans[j] = i-j
+#             st.append(i)
         
+#         return ans
+
+        mx = 0
+    
+        for i in range(n-1,-1,-1):
+            cur_temp = temperatures[i]
+            
+            if cur_temp>=mx:
+                mx = cur_temp
+                continue
+            
+            d = 1
+            
+            while temperatures[i+d]<=cur_temp:
+                d+=ans[i+d]
+            
+            ans[i] = d
         return ans
-        
