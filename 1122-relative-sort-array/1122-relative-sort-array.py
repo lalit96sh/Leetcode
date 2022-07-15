ab23901832c22da7431fcfdb420ed884daddb134
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-07-15 23:18:13 +0530

    Time: 38 ms (94.31%), Space: 13.9 MB (97.81%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -2,9 +2,11 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         
         mp = {}
-        mx = max(arr2)
+        mx = 0
         for i,n in enumerate(arr2):
             mp[n] = i
+            if n>mx:
+                mx = n
         
         
         arr1.sort(key=lambda x: mp[x] if x in mp else mx+1+x)
