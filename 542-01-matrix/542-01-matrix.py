35fc0db4b5694c459714858b7670fe3bc824e306
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-07-21 14:43:14 +0530

    Time: 1208 ms (29.77%), Space: 17 MB (78.11%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -5,25 +5,29 @@ class Solution:
         m = len(mat)
         n = len(mat[0])
         
-        dis = [[float("inf")]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j]==1:
+                    mat[i][j] = float("inf")
+                    
         
         for i in range(m):
             for j in range(n):
-                if mat[i][j] == 0:
-                    dis[i][j] = 0
-                    continue
+                # if mat[i][j] == 0:
+                #     dis[i][j] = 0
+                #     continue
                 
                 if i>0:
-                    dis[i][j] = min(dis[i][j],1+dis[i-1][j])
+                    mat[i][j] = min(mat[i][j],1+mat[i-1][j])
                 if j>0:
-                    dis[i][j] = min(dis[i][j],1+dis[i][j-1])
+                    mat[i][j] = min(mat[i][j],1+mat[i][j-1])
                     
             
         for i in range(m-1,-1,-1):
             for j in range(n-1,-1,-1):
                 if i<m-1:
-                    dis[i][j] = min(dis[i][j],1+dis[i+1][j])
+                    mat[i][j] = min(mat[i][j],1+mat[i+1][j])
                 if j<n-1:
-                    dis[i][j] = min(dis[i][j],1+dis[i][j+1])
+                    mat[i][j] = min(mat[i][j],1+mat[i][j+1])
                     
-        return dis
+        return mat
