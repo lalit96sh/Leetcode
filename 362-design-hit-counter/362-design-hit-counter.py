4b9fc3a3f2b979b044dd984726a4aa0329640188
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-07-24 11:38:28 +0530

    Time: 50 ms (44.22%), Space: 13.9 MB (61.71%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -0,0 +1,27 @@
+class HitCounter:
+
+    def __init__(self):
+        self.window = collections.deque()
+        self.allowed_window_size = 300
+        
+
+    def hit(self, timestamp: int) -> None:
+        self.clear(timestamp)
+        self.window.append(timestamp)
+        
+    def clear(self, timestamp):
+        while self.window and self.window[0]<=timestamp-self.allowed_window_size:
+            self.window.popleft()
+            
+
+    def getHits(self, timestamp: int) -> int:
+        
+        self.clear(timestamp)
+        return len(self.window)
+        
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
