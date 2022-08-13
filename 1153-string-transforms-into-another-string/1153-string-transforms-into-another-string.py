6529eb05a6b5e24fbf3468bb91c7160fa55103cc
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-08-13 21:31:43 +0530

    Time: 36 ms (86.96%), Space: 14 MB (21.01%) - LeetHub

--- 1153-string-transforms-into-another-string/1153-string-transforms-into-another-string.py
+++ 1153-string-transforms-into-another-string/1153-string-transforms-into-another-string.py
@@ -4,15 +4,19 @@ class Solution:
         if str1==str2:
             return True
         mp = {}
-        seen = set()
+        nodes = set("abdefghijklmnopqrstuvwxyz")
+        non_zero_indegree = set()
         for i,st in enumerate(str1):
             if st in mp and mp[st]!=str2[i]:
                 return False
-
             mp[st]=str2[i]
-            seen.add(str2[i])
+            non_zero_indegree.add(str2[i])
+                
+        
+            
+            
             
         
-        return len(seen)<26
+        return bool(nodes-non_zero_indegree)
             
         
