9f0939945f621ab402435aac4a32f508148850ae
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-08-17 19:17:15 +0530

    Time: 50 ms, Memory: 14.8 MB - LeetHub

--- sum-of-left-leaves/sum-of-left-leaves.py
+++ sum-of-left-leaves/sum-of-left-leaves.py
@@ -14,20 +14,11 @@ class Solution:
                 return 0,0
             l,ls = dfs(root.left)
             r,rs = dfs(root.right)
-            # print("----")
-            # print(root.val)
-            # print(l,ls,r,rs)
-            sm = 0
+
             if l==0 and r==0:
-                # print(1,root.val)
                 return 1,root.val
             
-            # if l==1:
-            sm+=ls
-            if r>1:
-                sm+=rs
-            # print(1+max(l,r),sm)
-            return 1+max(l,r),sm
+            return 1+max(l,r),ls+(rs if r>1 else 0)
         
         _,ans = dfs(root)
         return ans if _>1 else 0
