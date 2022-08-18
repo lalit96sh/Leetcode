19b18ff93aaa9abc44789af7d59a58682f069f38
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-08-19 00:17:30 +0530

    Time: 125 ms (85.14%), Space: 13.9 MB (80.21%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,22 +1,31 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        if 2*k >= len(prices): 
-            return sum(max(0, prices[i]-prices[i-1]) for i in range(1, len(prices)))
-        dp = [0]*(k+1)
-        mn = [prices[0]]*(k+1)
+        
+        def get_profit_for_inf():
+            last = float("inf")
+            ans = 0
+            for x in prices:
+                ans+= max(0,x-last)
+                last = x
+            return ans
         
         n = len(prices)
-        # print(mn)
-        # print(dp)
-        # print("--------------------")
-        for i in range(1,n):
+        if 2*k>=n:
+            return get_profit_for_inf()
+        
+        
+        mn = [prices[0]]*(k+1)
+        ans = [0]*(k+1)
+        
+        
+            
+        for val in prices[1:]:
             for kk in range(1,k+1):
-                if mn[kk]>prices[i]-dp[kk-1]:
-                    mn[kk] = prices[i]-dp[kk-1]
+                if val-ans[kk-1]<mn[kk]:
+                    mn[kk] = val-ans[kk-1]
                 else:
-                    dp[kk] = max(dp[kk],prices[i]-mn[kk])
-                
-        return dp[k]
+                    ans[kk] = max(ans[kk],val-mn[kk])
+        return ans[k]
             
         
         
