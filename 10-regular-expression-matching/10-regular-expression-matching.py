13f8600a00fbfe805659fbf32aed3d8b098d4d9d
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-08-19 00:41:50 +0530

    Time: 55 ms (85.07%), Space: 13.9 MB (91.26%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,27 +1,32 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        
         ns = len(s)
         np = len(p)
+
         
-        dp = [[False]*(np+1)for _ in range(ns+1)]
+        dp = [[False]*(np+1) for _ in range(ns+1)]
         
         dp[0][0] = True
         
         for i in range(ns+1):
+            
             for j in range(1,np+1):
+                
                 if i==0:
                     if p[j-1]=="*":
-                        dp[i][j]=dp[i][j-2]
+                        dp[i][j] = dp[i][j-2]
                     continue
-
-                if p[j-1]==s[i-1] or p[j-1]==".":
-                    dp[i][j]=dp[i-1][j-1]
+                    
+                if s[i-1]==p[j-1] or p[j-1]==".":
+                    dp[i][j] = dp[i-1][j-1]
+                    
                 elif p[j-1]=="*":
-                    dp[i][j]=dp[i][j-2] # zero occurence
-                    if ((p[j-2]==s[i-1]) or (p[j-2]==".")):
+                    dp[i][j] = dp[i][j-2]
+                    
+                    if s[i-1]==p[j-2] or p[j-2]==".":
                         dp[i][j] = dp[i][j] or dp[i-1][j]
-        # print(dp)
-        for i in range(ns+1):
-            print(dp[i])
+                        
         return dp[ns][np]
+                    
                 
