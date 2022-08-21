29e73fac1280b9ed1932037adfa1d300f36720dc
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-08-22 02:00:03 +0530

    Time: 3369 ms (30.86%), Space: 144.1 MB (16.29%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        
+        # dest_flag = False
+        # src_flag = False
+        
+        def lca(root):
+            
+            if not root:
+                return "",False,False
+            
+            
+            l,lsrc_flag,ldest_flag = lca(root.left)
+            
+            if lsrc_flag and ldest_flag:
+                return l,True,True
+            
+            
+            
+            r,rsrc_flag,rdest_flag = lca(root.right)
+            
+            if rsrc_flag and rdest_flag:
+                return r,True,True
+            
+            
+            dest_flag = rdest_flag or ldest_flag
+            src_flag = lsrc_flag or rsrc_flag
+            
+            
+                
+            if src_flag:
+                st = l+"U" if lsrc_flag else r+"U"
+            else:
+                st = ""
+                
+            if dest_flag:
+                st += ("L"+ l if ldest_flag else "R"+r)
+            else:
+                st += ""
+
+                    
+            return st, root.val==startValue or src_flag, root.val==destValue or dest_flag
+            
+        return lca(root)[0]
+            
+            
+                
+                
+            
+                
+            
+        
