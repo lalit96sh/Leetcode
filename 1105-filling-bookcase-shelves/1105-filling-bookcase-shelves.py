f000cb23dcf1c82345b9f23f820f8a1145f6331d
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-08-21 23:58:05 +0530

    Time: 107 ms (21.01%), Space: 17.2 MB (5.04%) - LeetHub

---
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+        
+        n = len(books)
+        memo = {}
+        def dfs(start,width_remaining,highest):
+            if start==n:
+                return 0
+            if (start,width_remaining,highest) in memo:
+                return memo[(start,width_remaining,highest)]
+            w,h = books[start]
+            mx_h = max(h,highest)
+            mn =float("inf")
+            if shelfWidth!=width_remaining:
+                mn = min(mn,highest+dfs(start,shelfWidth,0))
+            
+            if width_remaining>=w:
+                mn = min(mn,dfs(start+1,width_remaining-w,mx_h))
+                
+
+
+            memo[(start,width_remaining,highest)] = max(mn,highest,h)
+            return memo[(start,width_remaining,highest)]
+        
+        return dfs(0,shelfWidth,0)
+            
+            
+            
+        
