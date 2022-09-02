7d2030b81785c43332116cb8dc06ee2506ad2753
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-09-03 01:08:48 +0530

    Time: 319 ms (6.84%), Space: 16 MB (44.92%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,29 +1,46 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
-        n = len(height)
-        left_max = 0
-        right_max = 0
+#         n = len(height)
+#         l = 0
+#         r = n-1
         
-        left = 0
-        right = n-1
+#         leftmax = rightmax = 0
+#         ans = 0
         
+#         while l<r:
+            
+#             if height[l]<height[r]:
+                
+#                 if height[l]<=leftmax:
+#                     ans += (leftmax-height[l])
+#                 else:
+#                     leftmax = height[l]
+#                 l+=1
+#             else:
+                
+#                 if rightmax>=height[r]:
+#                     ans+=(rightmax-height[r])
+#                 else:
+#                     rightmax = height[r]
+#                 r-=1
+#         return ans
+        st = []
         ans = 0
-
-        while left < right:
-            if height[left] < height[right]:
-                if height[left]>left_max:
-                    left_max = height[left]
-                else:
-                    ans+=(left_max - height[left])
-                left+=1
-            else:
-                if height[right]>right_max:
-                    right_max = height[right]
-                else:
-                    ans+=(right_max - height[right])
-                right-=1
-            
+        for i,h in enumerate(height):
+            while st and height[st[-1]]<h:
+                cur = st.pop()
+                if not st:
+                    break
+                prev = st[-1]
+                
+                length = i-prev-1
+                
+                height_diff = abs(height[cur]-min(h,height[prev]))
+                
+                ans+=height_diff*length
+            st.append(i)
         return ans
-            
+                
+                
         
