71ec0b6af6937286a3f16acee1e85d9de7d2a898
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-09-03 00:56:36 +0530

    Time: 1482 ms (14.99%), Space: 31.6 MB (16.54%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -3,9 +3,20 @@ class Solution:
         
         st = []
         
-        for i,h in enumerate(heights):
-            while st and heights[st[-1]]<=h:
-                st.pop()
-            st.append(i)
+#         for i,h in enumerate(heights):
+#             while st and heights[st[-1]]<=h:
+#                 st.pop()
+#             st.append(i)
         
-        return st
+#         return st
+        n = len(heights)
+        mx = float("-inf")
+        for i in range(n-1,-1,-1):
+            if heights[i]<=mx:
+                continue
+            
+            st.append(i)
+            mx = heights[i]
+            
+        return st[::-1]
+            
