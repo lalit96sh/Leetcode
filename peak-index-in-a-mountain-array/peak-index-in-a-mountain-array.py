ef50d8dbe08daef7b020e8e450965d4a397b1985
Lalit Sharma <86369339+lalit96sh@users.noreply.github.com>
2022-09-06 15:43:17 +0530

    Time: 998 ms, Memory: 27.8 MB - LeetHub

--- peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -4,14 +4,12 @@ class Solution:
         i = 0
         j = len(arr)-1
         
-        while i<=j:
+        while i<j:
             mid = (i+j)//2
             
-            if arr[mid-1]<arr[mid]>arr[mid+1]:
-                return mid
-            
-            elif arr[mid-1]<arr[mid]<arr[mid+1]:
-                i=mid
+            if arr[mid]<arr[mid+1]:
+                i = mid+1
             else:
-                j=mid
-        return -1
+                j = mid
+            
+        return i
